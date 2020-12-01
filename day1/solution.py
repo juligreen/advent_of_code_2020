e2e83b0dab0bb2dd5f970e51cb2df7b3ad97cedb
Julius Dehner <julius.dehner@gmail.com>
2020-12-01 15:03:38 +0100

    Add part2 of dayone

--- day1/solution.py
+++ day1/solution.py
@@ -9,3 +9,9 @@ for number in numbers:
         if (number + number2) == 2020:
             print(f"number1: {number}\nnumber2: {number2}")
             print(number * number2)
+
+for number in numbers:
+    for number2 in numbers:
+        for number3 in numbers:
+            if (number + number2 + number3) == 2020:
+                print(number * number2 * number3)
